61a3bbd5dd61b9ff4fc0c6b8d02cc602ba7ea6ae
Pavel Koch <kouks.koch@gmail.com>
2015-09-13 20:28:59 +0200

    db config

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,21 @@
  */
 
 return array(
-    // ...
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+        'aliases' => array(
+            'db' => 'Zend\Db\Adapter\Adapter',
+        ),
+    ),
+    'db' => array(
+        'driver'    => 'pdo',
+        'dsn'       => 'mysql:dbname=trinec;host=localhost',
+        'username'  => 'root',
+        'password'  => '',
+    	'driver_options' => array(
+                        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+         ),
+    ),
 );
